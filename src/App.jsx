08fccd9cfa93e58bbad5521cabbd7fa556f1360a
velascorussel <jrevelasco.student@gmail.com>
2025-09-20 16:12:27 +0800

    Add Componenets

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="container">
+
+      <div className="top">
+        <div className="box orange"></div>
+        <div className="box orange inner-flex">
+          <div className="inner pink light">Russel John</div>
+          <div className="inner pink dark">Velasco</div>
+        </div>
+        <div className="box orange"></div>
+      </div>
+
+
+      <div className="middle">
+        <div className="large pink dark"></div>
+        <div className="side">
+          <div className="small pink dark"></div>
+          <div className="small pink dark"></div>
+        </div>
+      </div>
+
+
+      <div className="bottom-section">
+        <div className="box purple"></div>
+        <div className="box orange bottom-orange">C-PCIT9LEC</div>
+        <div className="box orange bottom-orange">IT-3B</div>
+        <div className="box purple"></div>
+      </div>
+    </div>
+  );
+}
